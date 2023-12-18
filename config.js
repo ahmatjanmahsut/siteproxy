4170882fdd8e90009a567da6c356bd76e4559c46
ahmatjon <1479626534@qq.com>
2023-12-18 19:46:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.workers.dev',
+    serverName: 'siteproxy-pc8zuv5of-ahmatjans-projects.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
